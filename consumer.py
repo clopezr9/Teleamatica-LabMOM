e6fa7ed0cf52c93dcbe4e60f1dc8c2fc27f65a77
clopezr9 <clopezr9@eafit.edu.co>
2021-09-11 19:41:58 -0500

    MOM

--- consumer.py
+++ consumer.py
@@ -3,11 +3,11 @@ import sys
 
 if (len(sys.argv) != 5):
     print('You need to introduce server IP, Port, RabbitMQ user and password')
-
-dir_ip = sys.argv[1]
-port = sys.argv[2]
-user = sys.argv[3]
-password = sys.argv[4]
+else:
+    dir_ip = sys.argv[1]
+    port = sys.argv[2]
+    user = sys.argv[3]
+    password = sys.argv[4]
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(dir_ip, port, '/', pika.PlainCredentials(user, password)))
 channel = connection.channel()
--- producer.py
+++ producer.py
@@ -3,19 +3,18 @@ import sys
 
 if (len(sys.argv) != 5):
     print('You need to introduce server IP, Port, RabbitMQ user and password')
+else:
+    dir_ip = sys.argv[1]
+    port = sys.argv[2]
+    user = sys.argv[3]
+    password = sys.argv[4]
 
-dir_ip = sys.argv[1]
-port = sys.argv[2]
-user = sys.argv[3]
-password = sys.argv[4]
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(dir_ip, port, '/', pika.PlainCredentials(user, password)))
 channel = connection.channel()
 print("Runnning Producer Application...")
 print("Type in a task to be send")
 
-while channel:
+while True:
     task = input()
     channel.basic_publish(exchange='my_exchange', routing_key='test', body=task)
-
-connection.close()
